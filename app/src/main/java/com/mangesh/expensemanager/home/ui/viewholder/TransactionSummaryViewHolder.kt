68649fcd7187cbd06f4597bfa99eb63012dcbf88
yadavmangesh <mangesh.yadav@smallcase.com>
2023-05-27 07:46:34 +0530

    progress made zero for negative balance

--- app/src/main/java/com/mangesh/expensemanager/home/ui/viewholder/TransactionSummaryViewHolder.kt
+++ app/src/main/java/com/mangesh/expensemanager/home/ui/viewholder/TransactionSummaryViewHolder.kt
@@ -15,9 +15,9 @@ class TransactionSummaryViewHolder(private val binding: LayoutTransactionSummary
             tvIncome.text = "$ ${model.totalIncome}"
             tvTotal.text = "$ " + (model.totalIncome - model.totalExpense)
             progressHorizontal.max = 100
-            if (model.totalIncome == 0 && model.totalExpense == 0){
+            if (model.totalIncome == 0 && model.totalExpense == 0 || (model.totalIncome - model.totalExpense) < 0) {
                 progressHorizontal.progress = 0
-            }else{
+            } else {
                 if (model.totalIncome != 0) {
                     val progress = model.totalExpense * 100 / model.totalIncome
                     progressHorizontal.progress = progress
